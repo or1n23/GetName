5c144ad5beaaa2b2c65896bd2699da4d7ddf32e6
Oliver <or1n23@soton.ac.uk>
2023-10-08 17:03:40 +0100

    Main program works, needs tidying

--- GetName.java
+++ GetName.java
@@ -2,12 +2,11 @@ import java.net.*;
 import java.io.*;
 
 public class GetName {
-    
     public static void main(String[] args) throws MalformedURLException {
         
         String id = "";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter a unique id:  ");
+        System.out.println("Enter a unique id:");
 
         try {
             id = br.readLine();
@@ -16,7 +15,30 @@ public class GetName {
         }
 
         String address = "https://www.ecs.soton.ac.uk/people/" + id;
-        System.out.println(address);
         URL url = URI.create(address).toURL();
+        URLConnection urlConnection = null;
+        HttpURLConnection connection = null;
+        BufferedReader in = null;
+
+        try {
+            urlConnection = url.openConnection();
+            connection = (HttpURLConnection)urlConnection;
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        } catch(IOException exception) {
+            System.err.println("Invalid URL");
+        }
+
+        String current, nameLine, name;
+        try {
+            while ((current = in.readLine()) != null) {
+                if (current.indexOf("\"@type\": \"Person\"") != -1) {
+                    nameLine = in.readLine();
+                    name = nameLine.substring(21, nameLine.length() - 2);
+                    System.out.println(name);
+                }
+            }
+        } catch (IOException exception) {
+            System.err.println("IO Exception");
+        }
     }
 }
