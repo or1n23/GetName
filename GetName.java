545804e1dafd73f501a231316036777081898680
Oliver <or1n23@soton.ac.uk>
2023-10-08 17:41:15 +0100

    Tidied up.

--- GetName.java
+++ GetName.java
@@ -1,44 +1,48 @@
 import java.net.*;
 import java.io.*;
 
+
 public class GetName {
-    public static void main(String[] args) throws MalformedURLException {
-        
-        String id = "";
+    static final int BEGIN_IDX           = 21;
+    static final int END_IDX             = 2;
+    static final String ADDRESS_TEMPLATE = "https://www.ecs.soton.ac.uk/people/";
+
+    static URL createURL() throws MalformedURLException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter a unique id:");
+        System.out.println("Enter a unique ID:");
+        String id = br.readLine();
+        String address = ADDRESS_TEMPLATE + id;
 
-        try {
-            id = br.readLine();
-        } catch (IOException ioe) {
-            System.err.println("There was an input error.");
-        }
+        return URI.create(address).toURL();
+    }
 
-        String address = "https://www.ecs.soton.ac.uk/people/" + id;
-        URL url = URI.create(address).toURL();
-        URLConnection urlConnection = null;
-        HttpURLConnection connection = null;
-        BufferedReader in = null;
+    static void findName(URL url) throws IOException {
+        URLConnection urlConnection = url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+    
+        String currentLine, nameLine, name;
+        Boolean nameFound = false;
 
-        try {
-            urlConnection = url.openConnection();
-            connection = (HttpURLConnection)urlConnection;
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        } catch(IOException exception) {
-            System.err.println("Invalid URL");
-        }
-
-        String current, nameLine, name;
-        try {
-            while ((current = in.readLine()) != null) {
-                if (current.indexOf("\"@type\": \"Person\"") != -1) {
-                    nameLine = in.readLine();
-                    name = nameLine.substring(21, nameLine.length() - 2);
-                    System.out.println(name);
-                }
+        while ((currentLine = in.readLine()) != null) {
+            if (currentLine.indexOf("\"@type\": \"Person\"") != -1) {
+                nameLine = in.readLine();
+                name = nameLine.substring(BEGIN_IDX, nameLine.length() - END_IDX);
+                System.out.println(name);
+                nameFound = true;
             }
+        }
+        if (!nameFound) {
+            System.out.println("Name not found. Please check the inputted unique ID.");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            findName(createURL());
+        } catch (MalformedURLException exception) {
+            System.out.println("Invalid URL.");
         } catch (IOException exception) {
-            System.err.println("IO Exception");
+            System.out.println("IO exception occurred.");
         }
     }
 }
