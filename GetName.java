1b58c7c3a59d3d5db2aa01c01256b5ee267e2a5e
or1n23 <147314719+or1n23@users.noreply.github.com>
2023-10-08 12:56:19 +0100

    Create GetName.java

---
+++ GetName.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+public class GetName {
+    
+    public static void main(String[] args) throws MalformedURLException {
+        
+        String id = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a unique id:  ");
+
+        try {
+            id = br.readLine();
+        } catch (IOException ioe) {
+            System.err.println("There was an input error.");
+        }
+
+        String address = "https://www.ecs.soton.ac.uk/people/" + id;
+        URL url = URI.create(address).toURL();
+    }
+}
